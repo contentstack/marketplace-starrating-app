4f32be7cd03eb00086b562c852d97b27975b52d8
MaitriGurey1 <maitri.gurey@contentstack.com>
2023-07-05 17:11:35 +0530

    added heap events for app load and failure

--- ui/src/common/constants/index.ts
+++ ui/src/common/constants/index.ts
@@ -8,4 +8,9 @@ const constants: any = {
   ],
 };
 
+export const eventNames = Object.freeze({
+  APP_INITIALIZE_SUCCESS: "App Loaded Successfully",
+  APP_INITIALIZE_FAILURE: "App Load Failure",
+});
+
 export default constants;
