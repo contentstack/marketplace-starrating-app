27ed64482356e00910f2755052ad8a7b325783bf
MaitriGurey1 <maitri.gurey@contentstack.com>
2023-07-13 11:14:15 +0530

    fix : added global properties to DDR

---
+++ ui/src/common/functions/index.ts
@@ -0,0 +1,16 @@
+import Extension from "@contentstack/app-sdk/dist/src/extension";
+import { get, isEmpty, keys } from "lodash";
+
+function getAppLocation(sdk: Extension): string {
+  const locations = keys(sdk?.location);
+  let locationName = "";
+  for (let i = 0; i <= locations.length; i++) {
+    if (!isEmpty(get(sdk, `location.${locations[i]}`, undefined))) {
+      locationName = locations[i];
+      break;
+    }
+  }
+  return locationName;
+}
+
+export default getAppLocation;
