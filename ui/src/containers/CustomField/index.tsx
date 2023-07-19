281271f82753a4a544f3a0c550ab825363a912a0
MaitriGurey1 <maitri.gurey@contentstack.com>
2023-07-19 13:22:34 +0530

    fix : custom is not a function error

--- ui/src/containers/CustomField/index.tsx
+++ ui/src/containers/CustomField/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import ContentstackAppSdk from "@contentstack/app-sdk";
 import { isEmpty, get } from "lodash";
 import { Rating } from "react-simple-star-rating";
-import { TypeSDKData, TypeStarRatingData } from "../../common/types";
+import { TypeSDKData } from "../../common/types";
 import constants, { eventNames } from "../../common/constants";
 import useAnalytics from "../../common/hooks/useAnalytics";
 import "./styles.scss";
@@ -17,50 +17,48 @@ const CustomField: React.FC = function () {
   });
   // error tracking hooks
   const { setErrorsMetaData, trackError } = useJsErrorTracker();
-  const [ratingValue, setRatingValue] = useState<TypeStarRatingData>({
-    value: 0,
-  });
+  const [ratingValue, setRatingValue] = useState<number>(0);
   const { trackEvent } = useAnalytics();
   const { APP_INITIALIZE_SUCCESS, APP_INITIALIZE_FAILURE } = eventNames;
-  
+
   useEffect(() => {
-  ContentstackAppSdk.init()
-    .then(async (appSdk) => {
-      const config = await appSdk?.getConfig();
+    ContentstackAppSdk.init()
+      .then(async (appSdk) => {
+        const config = await appSdk?.getConfig();
 
-      setState({
-        config,
-        location: appSdk.location,
-        appSdkInitialized: true,
-      });
+        setState({
+          config,
+          location: appSdk.location,
+          appSdkInitialized: true,
+        });
 
-      const initialData = appSdk.location?.CustomField?.field?.getData();
+        const initialData = appSdk.location?.CustomField?.field?.getData();
 
-      if (initialData && !isEmpty(initialData) && initialData.value) {
-        setRatingValue({ value: initialData.value * 20 });
-      }
-      
-      trackEvent(APP_INITIALIZE_SUCCESS);
-      const appLocation: string = getAppLocation(appSdk);
-      const properties = {
+        if (!isEmpty(initialData)) {
+          setRatingValue(initialData);
+        }
+
+        trackEvent(APP_INITIALIZE_SUCCESS);
+        const appLocation: string = getAppLocation(appSdk);
+        const properties = {
           Stack: appSdk?.stack._data.api_key,
           Organization: appSdk?.currentUser.defaultOrganization,
           "App Location": appLocation,
           "User Id": get(appSdk, "stack._data.collaborators.0.uid", ""), // first uuid from collaborators
         };
         setErrorsMetaData(properties); // set global event data for errors
-    })
-    .catch((error) => {
-       trackError(error);
-      console.error(constants.appSdkError, error); 
-      trackEvent(APP_INITIALIZE_FAILURE);
-    });
-}, []);
-
+      })
+      .catch((error) => {
+        trackError(error);
+        console.error(constants.appSdkError, error);
+        trackEvent(APP_INITIALIZE_FAILURE);
+      });
+  }, []);
 
   const onChangeSave = (ratings: number) => {
-    setRatingValue({ value: ratings });
-    state.location?.CustomField?.field?.setData({ value: ratings / 20 });
+    setRatingValue(ratings);
+
+    state.location?.CustomField?.field?.setData(ratings);
   };
 
   return (
@@ -69,7 +67,7 @@ const CustomField: React.FC = function () {
         <Rating
           showTooltip
           allowHalfIcon
-          ratingValue={ratingValue.value}
+          ratingValue={ratingValue}
           onClick={onChangeSave}
           fillColorArray={constants.fillColorArray}
         />
